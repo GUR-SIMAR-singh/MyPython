0e801051a6a87c2bd395b8cbb1684b45b97c3022
Simar <simargur1708@gmail.com>
2026-06-21 00:09:13 -0700

    numeric data types exmp

---
+++ nmeric-dtype.py
@@ -0,0 +1,17 @@
+a=5 
+print (type(a)) # type is a built in function to check the data type of a variable
+
+b=-5 
+print (type(b))
+
+c=-5.5
+print (type(c))#FLOAT CAN ALSO STORE EXPONENTIAL VALUES
+
+d=1e-5
+print (type(d))
+
+e=1+2j #j=sqrt(-1) in python
+print (type(e)) #complex data type to store complex numbers in the form of a+
+#bj where a is the real part and b is the imaginary part of the complex number
+print (e.real) # to access the real part of the complex number
+print (e.imag) # to access the imaginary part of the complex number
